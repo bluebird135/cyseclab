bfad362521506a23482f80e6987cd36619f3b1c1
Marc Eichler <marc@fam-eichler.de>
2018-03-23 16:38:00 +0100

    Removed admin interface

--- cysec/urls.py
+++ cysec/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     #path('', include('pages.urls')),
     path('', include('urlcheck.urls')),
     path('checkurl/' , include('urlcheck.urls'))
